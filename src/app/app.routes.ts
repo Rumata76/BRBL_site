8fdfabf010005783ff3f7284dc7d41fc9013d2e9
Rumata76 <rumata76@hotmail.com>
2025-10-31 16:27:01 +0100

    front begin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,23 @@
 import { Routes } from '@angular/router';
-import { VideoComponent } from './video/video.component';
-import { TextComponent } from './text/text.component';
-import { InscriptionComponent } from './inscription/inscription.component';
-import {KazeComponent} from './kaze/kaze.component';
-import {KahiseComponent} from './kahise/kahise.component';
-import {KirundiComponent} from './kirundi/kirundi.component';
-import {UbuntuComponent} from './ubuntu/ubuntu.component';
+
+import { InscriptionComponent } from './features/inscription/inscription.component';
+import {KazeComponent} from './features/kaze/kaze.component';
+import {KahiseComponent} from './features/kahise/kahise.component';
+import {KirundiComponent} from './features/kirundi/kirundi.component';
+import {UbuntuComponent} from './features/ubuntu/ubuntu.component';
 
 
 export const appRoutes : Routes = [
-  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: '', redirectTo: 'course', pathMatch: 'full' },
+  { path: 'auth',
+    loadComponent: () => import('./auth/login/login.component').then(m =>m.LoginComponent)
+  },
+  { path: 'register',
+    loadComponent: () => import('./auth/register/register.component').then(m =>m.RegisterComponent)
+  },
+  { path: 'course',
+    loadComponent: () => import('./course/list/course-list.component').then(m =>m.CourseListComponent)
+  },
   { path: 'accueil', component: KazeComponent },
   { path: 'histoire', component: KahiseComponent },
   { path: 'kirundi', component: KirundiComponent },
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {JwtInterceptor} from './app/features/interceptors/jwt.interceptor';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)]
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([JwtInterceptor]))
+  ]
 }).catch(err => console.error(err));
